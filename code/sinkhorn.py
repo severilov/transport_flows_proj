cad7f5843a6e3d9b605adffa1532ef45dd1e1d92
vinnibuh <ivchyaroslav@gmail.com>
2021-12-06 17:33:43 +0300

    add docstrings for sinkhorn.Sinkhorn

--- code/sinkhorn.py
+++ code/sinkhorn.py
@@ -5,8 +5,36 @@ np.set_printoptions(suppress=True)
 
 
 class Sinkhorn:
-
+    """
+    Вроде похожая на корректную реализация Синхорна. +- Совпадает с псевдокодом
+    алгоритма 2 из https://arxiv.org/pdf/2005.11604.pdf. Отличия в том, что:
+    1) не обнаружено параметра gamma (видимо либо он уже зашит в косты, либо
+    его принимают равным 1;
+    2) не совсем ясно, что возвращается в методе rec_d_i_j. Вроде выглядит похожим на 
+    матрицу d_i_j из статьи, однако почему-то отсутствует нормализующий знаменатель.
+    Также почему-то результат умножают на общее число людей. Судя по тому, что авторы 
+    кода называют результат не d_i_j, а r, и метод называется rec_d_i_j, имеет значение, 
+    немного отличающееся от d_i_j из статьи.
+    3) Также, при сматчивании кода (если вдруг кто-то зачем-то этим решит заняться) обнаружено,
+    что лучше матчить его не с перопределенными на странице 9 статьи lambda_W и lambda_L,
+    а с их оригинальными значениями, оттуда единица вылезет в коде.
+    """
     def __init__(self, L, W, people_num, iter_num, eps):
+        """
+        Initialize all parameters for Sinkhorn.
+        ----------
+        Arguments:
+            L: np.array
+                Should be 1-dimensional and have length equal to W.
+            W: np.array
+                Should be 1-dimensional and have length equal to L.
+            people_num: int
+                Total people num, needed for d_i_j matrix computation
+            iter_num: int
+                Number of iterations of algorithm.
+            eps: float
+                Stop criterion.
+        """
         self.L = L
         self.W = W
         assert (len(L) == len(W))
@@ -17,7 +45,30 @@ class Sinkhorn:
         self.multistage_i = 0
 
     def sinkhorn(self, k, cost_matrix, lambda_W_prev, lambda_L_prev):
-
+        """
+        Single iteration of Sinkhorn algorithm. Highly resembles algorithm 2 of 
+        https://arxiv.org/pdf/2005.11604.pdf. -1 comes from redefining 
+        lambda_W and lambda_L on top of page 9. Gamma parameter from algorithm seems 
+        to be either integrated into cost_matrix or equal 1.
+        Takes as input lambda's values on previous iteration, cost_matrix and iteration number.
+        Returns updated values of lambda_L and lambda_W.
+        ----------
+        Arguments:
+            k: int
+                number of iteration
+            cost_matrix: np.array
+                T_ij from paper. Shape is (n x n), where n = self.n
+            lambda_W_prev: np.array
+                Should be 1-dimensional and have length of self.n
+            lambda_L_prev: np.array
+                Should be 1-dimensional and have length of self.n
+        ----------
+        Returns:
+            lambda_L: np.array
+                1-dimensional array of length n.
+            lambda_W: np.array
+                1-dimensional array of length n.
+        """
         if k % 2 == 0:
             lambda_W = lambda_W_prev
             lambda_L = np.log(np.nansum(
@@ -33,6 +84,23 @@ class Sinkhorn:
         return lambda_W, lambda_L
 
     def iterate(self, cost_matrix):
+        """
+        Perform Sinkhorn iteration process on cost_matrix.
+        Receives as input cost_matrix T_ij from paper.
+        Returns lambda_L, lambda_W and some analogue of d_i_j matrix from paper.
+        ----------
+        Arguments:
+            cost_matrix: np.array
+                Should be 2-dimensional and have shape (n x n)
+        ----------
+        Returns:
+            r: np.array
+                2-dimensional array, shape (n x n)
+            lambda_L: np.array
+                1-dimensional array of length n.
+            lambda_W: np.array
+                1-dimensional array of length n.
+        """
         cost_matrix[cost_matrix == 0.0] = 100.0
 
         lambda_L = np.zeros(self.n)
@@ -54,8 +122,26 @@ class Sinkhorn:
         return r, lambda_L, lambda_W
 
     def rec_d_i_j(self, lambda_L, lambda_W, cost_matrix):
+        """
+        Calculate some analogue of matrix d_i_j from paper. Not very clear why, but
+        here we don't have normalizing denominator (sum of matrix values). Also
+        for some purpose matrix is multiplied by total number of people.
+        ----------
+        Arguments:
+            lambda_L: np.array
+                Should be 1-dimensional and have length of n
+            lambda_W: np.array
+                Should be 1-dimensional and have length of n
+            cost_matrix: np.array
+                Should be 2-dimensional and have shape (n x n)
+        ----------
+        Returns:
+            d_i_j: np.array
+                2-dimensional array, shape (n x n)
+        """
         er = np.exp(-1 - cost_matrix - (np.reshape(lambda_L, (self.n, 1)) + lambda_W))
-        return er * self.people_num
+        d_i_j = er * self.people_num
+        return d_i_j
 
 class SinkhornNew:
 
